db7d059527286cda3389217aa545d496e412495e
Yoshikawa Taichi <yoshikawa-taichi-dm@ynu.jp>
2022-01-07 19:29:38 +0900

    Update config

--- config.py
+++ config.py
@@ -1,109 +1,97 @@
 # Configuration
-# version 2.6 (2021/12/1)
+# version 1.0 (2021/12/24)
 
-import os
-import time  as tm          # standard library
-import numpy as np
-import shutil as st
-from joblib import Parallel, delayed
+import  os
+import  sys
+import  time    as tm
+import  numpy   as np
+from    utils   import log
 
 
-''' Configuration '''
 class Configuration:
+    '''Configuration
+    '''
 
     def __init__(self):
 
-        # Get current time
+        # Current time
         self.time = tm.strftime('%Y-%m-%d_%H-%M-%S')
 
         # Parallel Process
-        self.n_jobs             = 9                     # the number of CPU cores using for parallel processing
+        self.n_jobs             = -1                     # the number of CPU cores using for parallel processing
         '''
-            -1      : using all CPU cores(=n_cpus)
+        1       : sequential execution          (cpu usage rate: ■□□□□□□□□□, ■=1)
+        2       : 2 CPUs are used.              (cpu usage rate: ■■□□□□□□□□, ■=2)
+        n (>0)  : n CPUs are used.              (cpu usage rate: ■■■■□□□□□□, ■=n)
+        -1      : all CPUs are used.            (cpu usage rate: ■■■■■■■■■■, □=0)
+        -2      : all CPUs but one are used.    (cpu usage rate: ■■■■■■■■■□, □=1)
+        n (<0)  : all CPUs but -n-1 are used.   (cpu usage rate: ■■■■■■□□□□, □=-n-1)
         '''
 
-        """ Experimental Setting """
-        # problem
-        prob_sets      = [
-            # 'test',
-            '4-bitAdder',   '4-bitMultiplier',  '4-bitShiftRotator',
-            '8-bitAdder',   '8-bitMultiplier',
-            '16-bitAdder',  '16-bitMultiplier'
+        # Development Version
+        self.version            = '3.5'
+
+        # Problem (Function)
+        prob_sets      = {
+            # basic function
+            'basic' : ['F1','F2','F3','F4','F5','F6','F7','F8','F9'],
+            # LSGO2013 function
+            'lsgo2013'  : ['LSGO2013_F1', 'LSGO2013_F2', 'LSGO2013_F3', 'LSGO2013_F4', 'LSGO2013_F5', 'LSGO2013_F6', 'LSGO2013_F7', 'LSGO2013_F8', 'LSGO2013_F9', 'LSGO2013_F10', 'LSGO2013_F11', 'LSGO2013_F12', 'LSGO2013_F13', 'LSGO2013_F14', 'LSGO2013_F15'],
+            'lsgo2013-2div': [
+                # 2div - 1st problem set
+                ['LSGO2013_F2', 'LSGO2013_F3', 'LSGO2013_F4', 'LSGO2013_F5', 'LSGO2013_F6', 'LSGO2013_F7', 'LSGO2013_F8', 'LSGO2013_F9', 'LSGO2013_F10', 'LSGO2013_F11', 'LSGO2013_F12'],
+                # 2div - 2nd problem set
+                ['LSGO2013_F1', 'LSGO2013_F13', 'LSGO2013_F14', 'LSGO2013_F15']
             ]
-
-        prob_set = {
-            # bit
-            '4bit'  : ['4-bitAdder', '4-bitMultiplier', '4-bitShiftRotator'],
-            '8bit'  : ['8-bitAdder', '8-bitMultiplier'],
-            '16bit' : ['16-bitAdder', '16-bitMultiplier'],
-            # equivalent problem computation
-            'div2-1': ['4-bitAdder', '4-bitMultiplier', '4-bitShiftRotator', '16-bitAdder'],
-            'div2-2': ['8-bitAdder', '8-bitMultiplier', '16-bitMultiplier'],
-            # others
-            '4-8bit'  : ['4-bitAdder', '4-bitMultiplier', '4-bitShiftRotator', '8-bitAdder', '8-bitMultiplier'],
-            'part1-1': ['4-bitAdder', '4-bitMultiplier', '4-bitShiftRotator'],
-            'part1-2': ['8-bitAdder', '8-bitMultiplier', '16-bitAdder', '16-bitMultiplier'],
-            'part2': ['8-bitMultiplier', '16-bitAdder', '16-bitMultiplier']
         }
-
-        self.prob_name          = prob_set['part2']
+        self.prob_dim           = 1000
+        self.prob_name          = prob_sets['lsgo2013']
+        self.prob_env_noise     = 'off'                 # LSGO2013 benchmark noise (on/off)
         self.opt_type           = 'AutoComplete'        # min or max
+
         # Environmental setting
-        self.max_trial          = 5                     # max trials (11/25)
+        self.max_trial          = 25                    # max trials (11/25/31)
         self.max_evals          = 3_000_000             # max FEs (1_000_000/3_000_000/10_000_000)
         self.initial_seed       = 1                     # initial_seed ~ initial_seed + max_trial - 1
 
         # Optimizer setting
         ## General
-        self.opt_name           = 'CCVR'                # optimizer type (the same class name except for '-')
-        self.subopt_name        = 'ACO'                 # GA, ACO
-        self.ls_name            = '2-opt'               # None, 2-opt, 3-opt
-        # self.ls_name            = [None,'2-opt']        # None, 2-opt, 3-opt
-        self.version            = '3.5'                 # development version of the system
+        self.opt_name           = 'CCEA'                # optimizer name
+        self.subopt_name        = 'PSO'                 # PSO, GA
         self.max_pop            = 10                    # population size (10)
-        self.blank_opt          = '2-stage'             # on, off, 2-stage
-        self.switch_timing      = 0.5                   # 2-stage parameter
-        # self.switch_timing      = [0., 0.2, 0.4, 0.6, 0.8, 1.0]
         ## GA Parameter
         self.crossover_rate     = 1.0
         self.mutation_rate      = 0.01
-        ## ACO Parameter
-        self.evaporation_rate   = 0.05
-        self.confidential_rate  = 0.95
-        self.beta               = 1
+        ## PSO Parameter
         ## CC Parameter
-        self.abbrev_solution    = False
+        self.max_div            = 100                   # division number
         self.fitness_assignment = 'best'                # best / current (*)
-        ## Grouping Parameter
+        self.group_name         = 'RDG3'                # SG / RDG3
         self.nonseps_div        = False                 # nonseps division
-        self.max_groupsize      = 100                   # max group size
-        ## Evaluator
-        self.max_reeval         = 10                    # re-eval
-        self.function_type      = 'adaptive'            # lasso (L1) or ridge (L2) or original or adaptive
+        self.max_nonseps_dim    = 100                   # max nonseparable dimension
+        self.mdc                = 2**(-52)              # machine dependence constant 2^(-52) in Python
+        self.ep_n               = 50                    # threshold min element size
+        '''
+        when ep_n = 1000, RDG3 is equivalent to the RDG or RDG2
+        '''
+        self.ep_s               = 100                   # threshold max dimension value to solve separable sets
 
         # I/O setting
         ## folder/file name assignment
         ### frame directory
         self.dirname    = {
             # [1] root folder
-            'env'   : '_env_easy',
-            'log'   : '_log',
-            'result': '_result',
-            'eval'  : 'evaluator',
-            'opt'   : 'optimizer',
-            # [2] _log folder
-            'input' : '_input',
-            'job'   : '_job',
-            'output': '_output',
-            # [3] _output/prob_name folder
-            'trial' : lambda n: f'trial-{n}',
-            # [4] _result/prob_name folder
-            'trials': 'trials',
-            # [5] optimizer folder
-            'group' : '_group'
+            'env'           : '_env',
+            'result'        : '_result',
+            'group'         : '_group',
+            # [2] _env folder
+            'cec2013lsgo'   : 'cec2013lsgo',
+            # [3] _result/prob_name folder
+            'trial'         : lambda n: f'trial-{n}',
+            'trials'        : 'trials'
         }
         ### experiment directory
-        self.comment    = f'AQFPLogicOpt_{self.shortenEvals()}-evals_{self.opt_name}-{self.subopt_name}_{self.time}'
+        self.comment    = f'{self.opt_name}-{self.subopt_name}_v{self.version}_{self.shortenEvals()}-evals_{self.time}'
         # self.comment    = f'debug-mode_{self.time}'
         self.expname    = {
             'input' : f'input_ver{self.version}_{self.comment}',
@@ -113,11 +101,8 @@ class Configuration:
         }
         ### experiment file
         self.filename   = {
-            # _log
-            'input' : lambda n: f'in_gen{n}.csv',
-            'job'   : lambda n: f'job_gen{n}.csv',
-            'output': lambda n: f'out_gen{n}.csv',
-            'data'  : lambda n: f'dat_gen{n}.csv',
+            # _env
+            'module': 'module.txt',
             # _result
             'result': lambda n: f'trial{n}_std.xlsx',
             'regular-log': lambda n,FEs: f'trial{n}_std_agg{FEs}.xlsx',
@@ -126,38 +111,25 @@ class Configuration:
             'result-all': lambda p,i: f'all_trials_{i}_{p}.xlsx',
             'result-stat': lambda p,i: f'stat-{i}_{p}.xlsx',
             'result-stat-image': lambda p,i: f'stat-{i}_{p}.png',
-            'setting': 'config.yml',
-            # evaluator
-            # 'eval'  : lambda p='_',q='_',n='_': f'AQFPQoRChecker-gnu_{p}_{q}_{n}.exe'
-            'eval'  : lambda p='_',q='_',n='_': f'AQFPQoRChecker-msvc_{p}_{q}_{n}.exe'
+            'setting': 'config.yml'
         }
 
         ## path
-        ### output root path
-        paths            = {
+        nas_root    = r'Yoshikawa\CCRDG3-PSO'
+        ## output root path
+        paths       = {
+            # current directory from Terminal
             'current'   : os.getcwd(),
-            'Home-OD'   : r'E:\OneDrive - 横浜国立大学\Documents\Development\Evolutionary Computation\AQFPLogic-Optimization',
-            'Laptop'    : r'C:\Users\taichi\Documents\SourceCode\AQFPLogic-Optimization',
-            'Laptop-OD' : r'C:\Users\Yoshikawa\OneDrive - 横浜国立大学\Documents\Development\Evolutionary Computation\AQFPLogic-Optimization',
-            'Lab-OD'    : r'C:\Users\Yoshikawa\OneDrive - 横浜国立大学\Documents\Development\Evolutionary Computation\AQFPLogic-Optimization',
-            'CLUSTER'   : r'C:\Users\nktlab\Desktop\Experiment\Yoshikawa\AQFPLogicOpt',
-            'RTX-01'    : r'D:\Experiment\Yoshikawa\AQFPLogicOpt'
+            # parent directory from this file
+            'pardir'    : os.path.dirname(__file__),
+            # nas folder
+            'nas'       : rf'\\192.168.11.2\nktlab\Desktop\Experiment\{nas_root}'
         }
-        # path output
-        # current or CLUSTER or RTX-01
-        # self.path_out   = paths['RTX-01']
-        self.path_out   = paths['current']
-        # nas settings
-        self.nas_mode   = False
-        # current
-        self.path_nas   = paths['current']
-        path_nas = self.path_nas if self.nas_mode else self.path_out
+        ### path output
+        self.path_out   = paths['pardir']
         ### frame path
-        self.env_path = os.path.join(path_nas, self.dirname['env'])
-        self.log_path = os.path.join(self.path_out, self.dirname['log'])
-        self.result_path = os.path.join(path_nas, self.dirname['result'])
-        self.opt_path = os.path.join(path_nas, self.dirname['opt'])
-        self.eval_path = os.path.join(self.path_out, self.dirname['eval'])
+        self.env_path = os.path.join(self.path_out, self.dirname['env'])
+        self.result_path = os.path.join(self.path_out, self.dirname['result'])
 
         ## Logger
         self.log = {
@@ -185,11 +157,22 @@ class Configuration:
         self.anime_interval = 5                                 # interval of animation output
 
     def setRandomSeed(self, seed=1):
+        '''Set seed value
+
+        Args:
+            seed (int, optional): Seed value. Defaults to 1.
+        '''
         self.seed           = seed
         self.rd             = np.random                         # random
         self.rd.seed(self.seed)                                 # set seed for random
 
     def shortenEvals(self):
+        '''Shorten fitness evaluations
+        EX) 10,000 -> 10k  /  1,000,000 -> 1m
+
+        Returns:
+            str: shorten evaluation expression
+        '''
         if self.max_evals//(10**3) > 0:
             if self.max_evals//(10**6) > 0:
                 short_eval = f'{self.max_evals//(10**6)}m'
@@ -200,74 +183,34 @@ class Configuration:
         return short_eval
 
     def addComment(self, comment:str):
+        '''Add comment
+
+        Args:
+            comment (str): comment for experiment log
+        '''
         if not comment.startswith('_'):
             comment = f'_{comment}'
         self.comment += comment
-        self.expname    = {
-            'input' : f'input_ver{self.version}_{self.comment}',
-            'job'   : f'job_ver{self.version}_{self.comment}',
-            'output': f'output_ver{self.version}_{self.comment}',
-            'result': f'result_ver{self.version}_{self.comment}'
-        }
 
     @staticmethod
-    def _deleteFolder(rem_path:str):
-        try:
-            if os.path.isdir(rem_path):
-                st.rmtree(rem_path)
-                print(f'[Config] Delete folder "{rem_path}".')
-            else:
-                print(f'[Config] Warning: Folder "{rem_path}" not exist.')
-        except Exception as e:
-            print(f'[Config] Error: {e}')
+    def setup(filename:str):
+        '''Setup PC
 
-    def deleteFolders(self, mode:str='log'):
-        # set absolute path
-        if mode=='log':
-            log_path = self.log_path
-        elif mode=='all':
-            root = os.getcwd()
-            log_path = os.path.join(root,self.dirname['log'])
+        Args:
+            filename (str): module file path or filename
+        '''
+        import subprocess as sp
+        PYTHON_VERSION:str  = '3.10'
+        cmd = ('py', f'-{PYTHON_VERSION}', '-m', 'pip', 'install', '-r', filename)
+        if os.path.isfile(filename):
+            proc = sp.Popen(cmd, shell=True)
+            result = proc.communicate()
+            log('Configuration', f'Setup finish!')
         else:
-            raise Exception(f'Error: invalid mode "{mode}" in deleteFolders')
-        # delete folders path
-        rem_paths = [
-            os.path.join(log_path,self.dirname['input'],self.expname['input']),
-            os.path.join(log_path,self.dirname['job'],self.expname['job']),
-            os.path.join(log_path,self.dirname['output'],self.expname['output'])
-        ]
-        if mode=='all':
-            rem_paths.append(
-                os.path.join(root,self.dirname['result'],self.expname['result'])
-            )
-        # Parallel Process
-        Parallel(n_jobs=self.n_jobs)( [delayed(self._deleteFolder)(rem_path)  for rem_path in rem_paths ] )
-        print('[Config] Delete Process Finished!')
+            log('Configuration', f'Error: Do not exist path "{filename}"', output=sys.stderr)
 
 
 ''' main '''
 if __name__ == '__main__':
-    print('< Folder deletion >')
-    # (1) working directory movement
-    from logger import Stdio
-    Stdio.moveWorkingDirectory()
-    # (2) delete folders
-    cnf = Configuration()
-    while(True):
-        print('■ Please input filename below:')
-        print('[EX1] input_verX.X_probname_optimizer_2021-06-14_02-30-15')
-        print('[EX2] job_verX.X_probname_optimizer_2021-06-14_02-30-15')
-        print('[EX3] output_verX.X_probname_optimizer_2021-06-14_02-30-15')
-        print('[EX4] result_verX.X_probname_optimizer_2021-06-14_02-30-15')
-        filename = input('filename : ')
-        if filename == '':
-            break
-        _discard, cnf.version, cnf.comment = filename.split('_',2)
-        cnf.version = cnf.version.replace('ver','')
-        cnf.expname    = {
-            'input' : f'input_ver{cnf.version}_{cnf.comment}',
-            'job'   : f'job_ver{cnf.version}_{cnf.comment}',
-            'output': f'output_ver{cnf.version}_{cnf.comment}',
-            'result': f'result_ver{cnf.version}_{cnf.comment}'
-        }
-        cnf.deleteFolders('all')
+    # setup PC for installing modules
+    Configuration.setup('_env/module.txt')
